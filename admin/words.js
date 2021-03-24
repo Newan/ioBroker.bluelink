0941b426f74a92df6b1963b7da140b5fc2d3e65d
Newan <info@newan.de>
2021-03-24 20:02:46 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'bluelink adapter settings': {
+        'en': 'Adapter settings for bluelink',
+        'de': 'Adaptereinstellungen für bluelink',
+        'ru': 'Настройки адаптера для bluelink',
+        'pt': 'Configurações do adaptador para bluelink',
+        'nl': 'Adapterinstellingen voor bluelink',
+        'fr': "Paramètres d'adaptateur pour bluelink",
+        'it': "Impostazioni dell'adattatore per bluelink",
+        'es': 'Ajustes del adaptador para bluelink',
+        'pl': 'Ustawienia adaptera dla bluelink',
+        'zh-cn': 'bluelink的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
