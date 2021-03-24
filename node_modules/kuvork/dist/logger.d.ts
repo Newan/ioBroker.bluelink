e1d28c6d71fd94dfbf5cf25b8ef9eb73bbeb3ac6
Newan <info@newan.de>
2021-03-24 22:16:57 +0100

    add .gitignore

--- node_modules/kuvork/dist/logger.d.ts
+++
@@ -1,3 +0,0 @@
-import * as winston from 'winston';
-declare const logger: winston.Logger;
-export default logger;
--- node_modules/kuvork/src/constants/america.ts
+++
@@ -1,8 +0,0 @@
-//TODO: Someone needs to figure out the Kia endpoints
-// we can then make a fork easier to maintain
-
-export const API_HOST = 'api.telematics.hyundaiusa.com';
-
-export const BASE_URL = `https://${API_HOST}`;
-export const CLIENT_ID = '815c046afaa4471aa578827ad546cc76';
-export const CLIENT_SECRET = 'GXZveJJAVTehh/OtakM3EQ==';
