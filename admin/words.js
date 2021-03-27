a5b29fd1f3885d2ecdb0c29f04320f19ddae337d
Newan <info@newan.de>
2021-03-27 23:38:27 +0100

    Translate all

--- admin/words.js
+++ admin/words.js
@@ -14,28 +14,100 @@ systemDictionary = {
         'pl': 'Ustawienia adaptera dla bluelink',
         'zh-cn': 'bluelink的适配器设置'
     },
-    'option1': {
-        'en': 'option1',
-        'de': 'Option 1',
-        'ru': 'Опция 1',
-        'pt': 'Opção 1',
-        'nl': 'Optie 1',
-        'fr': 'Option 1',
-        'it': 'opzione 1',
-        'es': 'Opción 1',
-        'pl': 'opcja 1',
-        'zh-cn': '选项1'
+    'Username': {
+        "en": "Username",
+        "de": "Nutzername",
+        "ru": "Имя пользователя",
+        "pt": "Nome de usuário",
+        "nl": "Gebruikersnaam",
+        "fr": "Nom d'utilisateur",
+        "it": "Nome utente",
+        "es": "Nombre de usuario",
+        "pl": "Nazwa Użytkownika",
+        "zh-cn": "用户名"        
     },
-    'option2': {
-        'en': 'option2',
-        'de': 'Option 2',
-        'ru': 'option2',
-        'pt': 'opção 2',
-        'nl': 'Optie 2',
-        'fr': 'Option 2',
-        'it': 'opzione 2',
-        'es': 'opcion 2',
-        'pl': 'Opcja 2',
-        'zh-cn': '选项2'
+    'Password': {
+        "en": "Password",
+        "de": "Passwort",
+        "ru": "Пароль",
+        "pt": "Senha",
+        "nl": "Wachtwoord",
+        "fr": "Mot de passe",
+        "it": "Parola d'ordine",
+        "es": "Contraseña",
+        "pl": "Hasło",
+        "zh-cn": "密码"        
+    },
+    'Pin': {
+        "en": "Personal identification number (pin)",
+        "de": "Persönliche Identifikationsnummer (PIN)",
+        "ru": "Персональный идентификационный номер (PIN-код)",
+        "pt": "Número de identificação pessoal (pin)",
+        "nl": "Persoonlijk identificatienummer (pincode)",
+        "fr": "Numéro d'identification personnel (pin)",
+        "it": "Numero di identificazione personale (pin)",
+        "es": "Número de identificación personal (pin)",
+        "pl": "Osobisty numer identyfikacyjny (pin)",
+        "zh-cn": "个人识别码（图钉）"        
+    },
+    'VIN': {
+        "en": "vehicle identification number (VIN)",
+        "de": "Fahrzeugidentifikationsnummer (VIN)",
+        "ru": "идентификационный номер автомобиля (VIN)",
+        "pt": "número de identificação do veículo (VIN)",
+        "nl": "voertuigidentificatienummer (VIN)",
+        "fr": "numéro d'identification du véhicule (VIN)",
+        "it": "numero di identificazione del veicolo (VIN)",
+        "es": "número de identificación del vehículo (VIN)",
+        "pl": "numer identyfikacyjny pojazdu (VIN)",
+        "zh-cn": "车辆识别号（VIN）"        
+    },
+    'Brand': {
+        "en": "Brand",
+        "de": "Marke",
+        "ru": "марка",
+        "pt": "marca",
+        "nl": "merk",
+        "fr": "marque",
+        "it": "marca",
+        "es": "marca",
+        "pl": "Marka",
+        "zh-cn": "品牌"        
+    },
+    'Request': {
+        "en": "Request per day",
+        "de": "Anfrage pro Tag",
+        "ru": "Запрос в день",
+        "pt": "Pedido por dia",
+        "nl": "Aanvraag per dag",
+        "fr": "Demande par jour",
+        "it": "Richiesta al giorno",
+        "es": "Solicitud por día",
+        "pl": "Żądanie dziennie",
+        "zh-cn": "每天请求"        
+    },
+    'Api settings': {
+        "en": "Api settings",
+        "de": "API-Einstellungen",
+        "ru": "Настройки API",
+        "pt": "Configurações de API",
+        "nl": "API-instellingen",
+        "fr": "Paramètres de l'API",
+        "it": "Impostazioni API",
+        "es": "Configuración de api",
+        "pl": "Ustawienia API",
+        "zh-cn": "api设置"
+    },
+    "Account settings": {
+        "en": "Account settings",
+        "de": "Kontoeinstellungen",
+        "ru": "Настройки учетной записи",
+        "pt": "Configurações de Conta",
+        "nl": "Account instellingen",
+        "fr": "Paramètres du compte",
+        "it": "Impostazioni dell'account",
+        "es": "Configuraciones de la cuenta",
+        "pl": "Ustawienia konta",
+        "zh-cn": "帐号设定"
     }
 };
