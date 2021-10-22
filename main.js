9d3d949fdf5d91bf2ed0d765a486aff2f4948121
TA2k <tombox2020@gmail.com>
2021-10-22 09:51:20 +0200

    filter only controls

--- main.js
+++ main.js
@@ -72,7 +72,9 @@ class Bluelink extends utils.Adapter {
         this.log.debug(JSON.stringify(state));
         this.log.debug(id);
         if (state) {
-
+            if (id.indexOf(".control." === -1)) {
+                return
+            }
             this.log.debug('New Event for state: ' + JSON.stringify(state));
             this.log.debug('ID: ' + JSON.stringify(id));
 
