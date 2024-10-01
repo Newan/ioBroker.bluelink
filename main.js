bd12aa766f7d7133672d7b0665761d18b510a19e
arteck <6681528+arteck@users.noreply.github.com>
2024-10-01 15:44:05 +0200

    chore: release v3.0.2
    * (arteck) corr for hev

--- main.js
+++ main.js
@@ -583,7 +583,7 @@ class Bluelink extends utils.Adapter {
                     ack: true
                 });
             } else {
-                if (newStatus.vehicleStatus.evStatus.hasOwnProperty('reservChargeInfos')) {
+                if (newStatus.vehicleStatus.hasOwnProperty('evStatus')) {
                     if (newStatus.vehicleStatus.evStatus.reservChargeInfos.targetSOClist[0].plugType == 1) {
                         //Slow  = 1  -> Index 0 ist slow
                         slow_charging = newStatus.vehicleStatus.evStatus.reservChargeInfos.targetSOClist[0].targetSOClevel;
