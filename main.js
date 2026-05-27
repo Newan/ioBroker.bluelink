f5b369dd6dc098ab044358f0313cd4cf0649ff3d
arteck <6681528+arteck@users.noreply.github.com>
2026-05-27 20:35:17 +0200

    upd

--- main.js
+++ main.js
@@ -646,8 +646,12 @@ class Bluelink extends utils.Adapter {
         let mm = today.getMonth() + 1;
         let dd = today.getDate();
 
-        if (dd < 10) { dd = `0${dd}`; }
-        if (mm < 10) { mm = `0${mm}`; }
+        if (dd < 10) {
+ dd = `0${dd}`; 
+}
+        if (mm < 10) {
+ mm = `0${mm}`; 
+}
 
         return `${yyyy}${mm}${dd}`;
     }
